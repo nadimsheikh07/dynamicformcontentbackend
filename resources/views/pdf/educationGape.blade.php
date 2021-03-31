7b4f6ca490de7c0cf3d75b563bf3dfe3250942cc
nadim <nadim.sheikh.07@gmail.com>
2021-03-31 22:39:58 +0530

    update

--- resources/views/pdf/educationGape.blade.php
+++ resources/views/pdf/educationGape.blade.php
@@ -24,11 +24,12 @@
 
     <p>eSa -----{{ $name }}----- iq=@iq=h@iRuh Jh -----{{ $fathername }}----- mez--------o"kZ] fuoklh -----{{ $address }}----- 'kiFkiwoZd fuosnu djrk@djrh gqa fd %&</p>
     <p>eSa 'kiFkiwoZd fuosnu djrk@djrh gwa fd esjs }kjk d{kk@dkslZ -----{{ $class }}----- lu~ -----{{ $classYear }}----- esa -----{{ $school }}----- ls fd;k FkkA</p>
-    <p>eSa 'kiFkiwoZd fuosnu djrk@djrh gwa fd lu~ -----{{ $fromYear }}----- ls -----{{ $toYear }}----- rd esjs }kjk fdlh Hkh ekU;rk izkIr fo’o fo|ky; ,oa egkfo|ky; esa v/;;u ugh fd;k x;k gS vkSj ;g le; esjk -----{{ $detail }}----- ds vUrxZr O;rhr gqvk ;gh esjs vUrjky dk dkj.k jgk gS ftl ckcr~ 'kiFk i= izLrqr fd;k tk jgk gSaA</p>
+    <p>eSa 'kiFkiwoZd fuosnu djrk@djrh gwa fd lu~ -----{{ $fromYear }}----- ls -----{{ $toYear }}----- rd esjs }kjk fdlh Hkh ekU;rk izkIr fo’o fo|ky; ,oa egkfo|ky; esa v/;;u ugh fd;k x;k gS vkSj ;g le; esjk -----{{ $detail }}----- ds vUrxZr O;rhr gqvk </p>
+    <p>;gh esjs vUrjky dk dkj.k jgk gS ftl ckcr~ 'kiFk i= izLrqr fd;k tk jgk gSaA</p>
     <p>eSa 'kiFkiwoZd fuosnu djrk@djrh gwa fd vc eSa dkslZ -----{{ $newSchool }}----- lu~ -----{{ $newSchoolYear }}----- esa izos’k pkgrk@pkgrh gwa@v/;;ujr gwaA</p>
 
 
-    <table style="margin-top: 50px;">
+    <table style="margin-top: 100px;">
         <tr>
             <td style="text-align:left;">
                 <p>LFkku % mn;iqj ¼jkt-½</p>
@@ -39,13 +40,14 @@
         </tr>
     </table>
 
-
-    <h3 style="text-align:center;">AA lR;kiu AA</h3>
+    <div style="margin-top: 50px;">
+        <h3 style="text-align:center;">AA lR;kiu AA</h3>
+    </div>
 
     <p>eSa -----{{ $name }}----- 'kiFkiwoZd lR;kfir djrk@djrh gwa fd mijksDr 'kiFk i= dh dye la[;k 1 ls vUr rd esjs futh Kku ls lgh ,oa lR; gS vkSj blesa dksbZ rF; fNik;k ugh x;k gSA</p>
 
 
-    <table style="margin-top: 50px;">
+    <table style="margin-top: 100px;">
         <tr>
             <td style="text-align:left;">
                 <p>LFkku % mn;iqj ¼jkt-½</p>
