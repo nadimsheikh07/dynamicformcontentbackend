0d885e5668acb8697c212028756de5273fac88a1
nadim <nadim.sheikh.07@gmail.com>
2021-03-31 22:10:11 +0530

    update css

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -14,7 +14,7 @@
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('public/css/app.css') }}" rel="stylesheet">
 </head>
 
 <body>
